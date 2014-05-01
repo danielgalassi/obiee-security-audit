04a03bff1d4a19f5654ea62e4b0dd658a7bdb040
danielgalassi@gmail.com <danielgalassi@gmail.com@cb81d4d0-347a-f961-effa-f90d8627f3e4>
2014-05-01 11:56:03 +0000

    Refactored, added comments and logging. Removed never-invoked code.

--- trunk/src/obiee/audit/webcat/objects/shared/Report.java
+++ trunk/src/obiee/audit/webcat/objects/shared/Report.java
@@ -7,22 +7,33 @@ import obiee.audit.webcat.core.PrivilegeAttribFile;
 import obiee.audit.webcat.core.SharedObject;
 import obiee.audit.webcat.core.WebCatalog;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.w3c.dom.Element;
 
 
 public class Report {
 
-//	private static final Logger logger = LogManager.getLogger(Report.class.getName());
+	private static final Logger logger = LogManager.getLogger(Report.class.getName());
 
+	/** the unscrambled name (not the title view) of the report as read by users when using OBIEE */
 	private String                     name = "";
+	/** the application role or user account set as the owner of the analysis */
 	private String                    owner = "";
 	private String                ownerType = "Application Role";
+	/** location of this analysis within the presentation catalogue */
 	private String                     path;
 	private boolean             ownerIsRole;
 	private boolean             ownerIsUser;
+	/** file containing the XML representation of the analysis */
 	private File	                   file;
+	/** the set of permissions granted to application roles and user accounts on the analysis */
 	private Vector <Permission> permissions;
 
+	/**
+	 * Creates a representation of the analysis including all retrieved fields and derived details.
+	 * @return an XML representation of the analysis
+	 */
 	public Element serialize() {
 		Element report = (WebCatalog.docWebcat).createElement("Report");
 		report.setAttribute("Name", name);
@@ -42,14 +53,31 @@ public class Report {
 		return report;
 	}
 
+	/**
+	 * Getter method
+	 * @return the location and unscrambled name of the analysis
+	 */
 	public String getFullUnscrambledName() {
 		return (path + "/" + name);
 	}
 
+	/**
+	 * Getter method
+	 * @return the unscrambled name of the analysis
+	 */
 	public String getName() {
 		return name;
 	}
 
+//	public void listPrivileges() {
+//		for (Permission permission : permissions) {
+//			permission.list();
+//		}
+//	}
+//
+	/**
+	 * Retrieves the application role or user set as the owner of this analysis
+	 */
 	private void setOwner() {
 		owner = SharedObject.getOwner(file);
 		ownerIsUser = WebCatalog.users.containsKey(owner);
@@ -60,11 +88,17 @@ public class Report {
 			owner = WebCatalog.users.get(owner);
 		}
 		if (!ownerIsUser && !ownerIsRole) {
+			logger.warn("The owner of {} could not be matched to a valid user or role", name);
 			ownerType = "Not Found";
 			owner = "Not Found (" + owner + ")";
 		}
 	}
 
+	/**
+	 * Constructor
+	 * @param path location of the report under shared folder
+	 * @param file XML file representing an OBIEE analysis
+	 */
 	public Report(String path, File file) {
 		if (file.canRead()) {
 			this.path = path;
