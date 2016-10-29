7e6ecabd87a86681b0f5629a4246d73b4797624a
Daniel Galassi <danielgalassi@gmail.com>
2016-10-29 20:56:47 +1100

    Defining main method as static

--- src/test/obiee/audit/webcat/engine/WebcatAuditTest.java
+++ src/test/obiee/audit/webcat/engine/WebcatAuditTest.java
@@ -1,13 +1,13 @@
 package test.obiee.audit.webcat.engine;
 
-import obiee.audit.webcat.engine.WebcatAudit;
-
 import org.testng.annotations.Test;
+
+import obiee.audit.webcat.engine.WebcatAudit;
 
 public class WebcatAuditTest {
 
 	@Test
-	public void main() {
+	public static void main() {
 		String s[] = {"-w=.\\sampleCases\\SampleApp","-privs","-dashboards"};
 		WebcatAudit audit = new WebcatAudit();
 		audit.main(s);
